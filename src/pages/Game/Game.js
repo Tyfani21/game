00f6287490c16bfd259942dea22dc57d459dae12
Tyfani21 <tyfani.abreu@gmail.com>
2021-11-25 09:23:56 -0400

    adiciona pg
    Signed-off-by: Tyfani21 <tyfani.abreu@gmail.com>

---
+++ src/pages/Game/Game.js
@@ -0,0 +1,45 @@
+import './Game.css';
+import { FaRegHeart } from 'react-icons/fa';
+import { useLocation } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const Game = () => {
+    const state = useLocation()
+    const id = state.state;
+
+    const [jogo, setJogo] = useState({})
+    const [mounted, setMounted] = useState(false)
+
+    const getData = async () => {
+        await axios.get(`/game/find${id}`)
+        .then(response => {
+            if(mounted){
+                setJogo(response.data)
+            }
+        })
+    }
+    useEffect(() =>{
+        setMounted(true)
+        getData()
+    }, [mounted])
+    return(
+        <div className='jg'>
+            <div className='jg-header'>
+                <img src={jogo.img} alt={jogo.nome}/>
+            </div>
+            <div className='game-wish'>
+        <h2>{jogo.nome}</h2>
+        <div className='game-like'>
+          <span>R$ {jogo.preco}</span>
+          <a href='!#'>
+            <FaRegHeart />
+          </a>
+        </div>
+      </div>
+      <h3>{jogo.year}</h3>
+      <p>{jogo.storyline}</p>
+    </div>
+    )
+}
+export default Game;
